9a212f54c1af8b8c8d13535a40443f9b5cb2f959
Timo Bechtel <hi@timobechtel.com>
2020-11-18 14:34:06 +0100

    feat(cli): add config file cli option

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,21 @@
 #!/usr/bin/env node
+import { program } from 'commander';
 import { parseConfigFile } from './config';
 import { generateApi } from './stapigen';
+const pkg = require('../package.json');
+
+program.version(pkg.version);
+program.option(
+	'-c, --config-file <file-path>',
+	'path to config file',
+	'stapigen.conf.js'
+);
+
+program.parse(process.argv);
 
 (async () => {
 	try {
-		const config = await parseConfigFile('stapigen.conf.js');
+		const config = await parseConfigFile(program.configFile);
 		await generateApi(config);
 	} catch (e) {
 		console.error(e);
