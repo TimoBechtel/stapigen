cade4a77acc04456158f567670a4626008379285
Timo Bechtel <hi@timobechtel.com>
2020-11-18 14:16:56 +0100

    fix(directoryparser): correctly parsing tags with more or less folders as specified in schema

--- src/directoryParser.ts
+++ src/directoryParser.ts
@@ -20,29 +20,40 @@ export function createDirectoryParser({
 	parsedSchema: string[];
 }) {
 	return async function traverseDirectory(
-		directory: string
+		directory: string,
+		subdirectory: string = ''
 	): Promise<DataObject[]> {
 		const dataList: DataObject[] = [];
+		const curDirectory = path.join(directory, subdirectory);
 
 		await Promise.all(
-			fs.readdirSync(directory, { withFileTypes: true }).map(async (file) => {
-				const currentFileOrDirPath = path.join(directory, file.name);
-				if (file.isDirectory()) {
-					dataList.push(...(await traverseDirectory(currentFileOrDirPath)));
-				} else {
-					const extension = path.extname(file.name);
-					const parse = parser?.find(compatibleWith(extension))?.parse;
-					if (!parse)
-						return console.warn(
-							`no parser found for file ${currentFileOrDirPath}`
+			fs
+				.readdirSync(curDirectory, {
+					withFileTypes: true,
+				})
+				.map(async (file) => {
+					const currentFileOrDirPath = path.join(curDirectory, file.name);
+					if (file.isDirectory()) {
+						dataList.push(
+							...(await traverseDirectory(
+								directory,
+								path.join(subdirectory, file.name)
+							))
 						);
+					} else {
+						const extension = path.extname(file.name);
+						const parse = parser?.find(compatibleWith(extension))?.parse;
+						if (!parse)
+							return console.warn(
+								`no parser found for file ${currentFileOrDirPath}`
+							);
 
-					const name = path.parse(file.name).name;
-					const tags = parseTags(directory, parsedSchema);
-					const data = await parseFile(currentFileOrDirPath, parse);
-					dataList.push({ name, tags, data });
-				}
-			})
+						const name = path.parse(file.name).name;
+						const tags = parseTags(subdirectory, parsedSchema);
+						const data = await parseFile(currentFileOrDirPath, parse);
+						dataList.push({ name, tags, data });
+					}
+				})
 		);
 
 		return dataList;
@@ -55,9 +66,9 @@ function parseTags(
 ): { [key: string]: string } {
 	const folders = dir.split('/');
 	const tags = {};
-	// there are probably more folders than tagNames, so we start reverse
-	reverse(parsedSchema).forEach((tagName, i) => {
-		tags[tagName] = folders[folders.length - 1 - i];
+	parsedSchema.forEach((tagName, i) => {
+		const currentFolder = folders[i];
+		if (currentFolder) tags[tagName] = currentFolder;
 	});
 	return tags;
 }
--- test/directoryParser.test.ts
+++ test/directoryParser.test.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import { parseSchema } from '../src/config';
 import { createDirectoryParser, DataObject } from '../src/directoryParser';
 import * as mock from 'mock-fs';
@@ -8,6 +7,7 @@ beforeAll(() => {
 		{
 			'test/example/input': {
 				'2020': {
+					'Imafileinsomeotherdir.md': 'nothing',
 					'04': {
 						'10': {
 							'lorem.md': `# lorem
@@ -16,6 +16,9 @@ Lorem ipsum dolor sit amet, consectetur adipisici elit,
 `,
 						},
 						'13': {
+							test: {
+								'somefile.md': '',
+							},
 							'incompatible.file': '',
 							'randomNotes.md': `# these are my random notes
 
@@ -46,56 +49,6 @@ test('parses complete directory', async () => {
 	const schema = ':year/:month/:day';
 	const exampleDir = 'test/example/input';
 
-	const expected: DataObject[] = [
-		{
-			data: {
-				content: `# these are my random notes
-
-- apples
-- oranges
-- bread
-`,
-				name: 'randomNotes.md',
-			},
-			name: 'randomNotes',
-			tags: {
-				year: '2020',
-				month: '04',
-				day: '13',
-			},
-		},
-		{
-			data: {
-				content: `# random thoughts
-
-Do we live in a simulation? Yes.
-`,
-				name: 'thoughts.md',
-			},
-			name: 'thoughts',
-			tags: {
-				year: '2020',
-				month: '04',
-				day: '13',
-			},
-		},
-		{
-			data: {
-				content: `# lorem
-
-Lorem ipsum dolor sit amet, consectetur adipisici elit,
-`,
-				name: 'lorem.md',
-			},
-			name: 'lorem',
-			tags: {
-				year: '2020',
-				month: '04',
-				day: '10',
-			},
-		},
-	];
-
 	const traverseDirectory = createDirectoryParser({
 		parser: [
 			{
@@ -108,8 +61,75 @@ Lorem ipsum dolor sit amet, consectetur adipisici elit,
 	const data = await traverseDirectory(exampleDir);
 
 	// data might not be in the same order
-	expect(data).toHaveLength(3);
-	expect(data).toContainEqual(expected[0]);
-	expect(data).toContainEqual(expected[1]);
+	expect(data).toHaveLength(5);
+	expect(data).toContainEqual({
+		data: {
+			name: 'Imafileinsomeotherdir.md',
+			content: 'nothing',
+		},
+		name: 'Imafileinsomeotherdir',
+		tags: {
+			year: '2020',
+		},
+	});
+	expect(data).toContainEqual({
+		data: {
+			content: `# these are my random notes
+
+- apples
+- oranges
+- bread
+`,
+			name: 'randomNotes.md',
+		},
+		name: 'randomNotes',
+		tags: {
+			year: '2020',
+			month: '04',
+			day: '13',
+		},
+	});
+	expect(data).toContainEqual({
+		data: {
+			content: `# random thoughts
+
+Do we live in a simulation? Yes.
+`,
+			name: 'thoughts.md',
+		},
+		name: 'thoughts',
+		tags: {
+			year: '2020',
+			month: '04',
+			day: '13',
+		},
+	});
+	expect(data).toContainEqual({
+		data: {
+			content: `# lorem
+
+Lorem ipsum dolor sit amet, consectetur adipisici elit,
+`,
+			name: 'lorem.md',
+		},
+		name: 'lorem',
+		tags: {
+			year: '2020',
+			month: '04',
+			day: '10',
+		},
+	});
+	expect(data).toContainEqual({
+		data: {
+			content: '',
+			name: 'somefile.md',
+		},
+		name: 'somefile',
+		tags: {
+			year: '2020',
+			month: '04',
+			day: '13',
+		},
+	});
 	expect(console.warn).toHaveBeenCalledTimes(1);
 });
